1ba084a5f9950b4657e51ec8eef06938584fbabc
mellivoro <2comp.eng00@gmail.com>
2022-10-08 04:35:20 +0300

    network

---
+++ TrackingSys/serthread.h
@@ -0,0 +1,28 @@
+#ifndef SERTHREAD_H
+#define SERTHREAD_H
+
+#include <QThread>
+#include <QObject>
+#include <QTcpSocket>
+
+
+class SerThread : public QThread
+{
+    Q_OBJECT
+public:
+    explicit SerThread(qintptr ID,QObject *parent = 0);
+    void run();
+
+  signals:
+      void error(QTcpSocket::SocketError socketerror);
+
+  public slots:
+      void readyRead();
+      void disconnected();
+
+  private:
+      QTcpSocket *socket;
+      qintptr socketDescriptor;
+};
+
+#endif // SERTHREAD_H
--- TrackingSys/server.h
+++ TrackingSys/server.h
@@ -3,12 +3,20 @@
 
 #include <QTcpServer>
 #include <QObject>
+#include "serthread.h"
 
 class Server : public QTcpServer
 {
     Q_OBJECT
 public:
    explicit Server(QObject *parent =0);
+    void startServer();
+signals:
+
+public slots:
+
+protected:
+    void incomingConnection(qintptr socketDescriptor);
 };
 
 #endif // SERVER_H
